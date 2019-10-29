d437403c69724f73ebb2a2bddf5d6affc67232fa
Dat Lam <lqdat2n@gmail.com>
2019-10-28 22:06:09 -0400

    Refactor:rename

--- src/strokerecognition/wsgi.py
+++ src/strokerecognition/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for strokeregconition project.
+WSGI config for strokerecognition project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strokeregconition.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strokerecognition.settings')
 
 application = get_wsgi_application()
