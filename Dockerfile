9dbc6e10bdbe231ba8d2be97f5b408746c6b50cc
Dat Lam <lqdat2n@gmail.com>
2019-11-08 21:10:36 -0800

    Integrate stroke recognition ML model

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-FROM python:3.7
+FROM ubuntu:18.04
+
+RUN apt-get update
+RUN apt-get -y install python3
+RUN apt-get -y install python3-pip
+RUN apt-get install -y libsm6 libxext6 libxrender-dev
 
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /strokerecognition
 WORKDIR /strokerecognition
 
 COPY . /strokerecognition
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 
 EXPOSE 8000
 
-CMD ["python", "src/manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python3", "src/manage.py", "runserver", "0.0.0.0:8000"]
--- src/strokeapi/views.py
+++ src/strokeapi/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
+from django.http import (
+    HttpResponseBadRequest
+)
 
 # Create your views here.
 from rest_framework import views
 from rest_framework.response import Response
 
+import tensorflow as tf
+
+from keras.models import load_model
+from keras.preprocessing import image
+import cv2
+from numpy import array
+import io
+from PIL import Image
+import json
+
+import base64
+
 from .serializers import StrokeApiSerializer
 
-import pickle
-import numpy as np
-from strokeapi.models import NLPModel
 
 print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
-model = NLPModel()
+new_model = load_model('src/lib/models/data.h5', compile=False)
 
-clf_path = 'src/lib/models/SentimentClassifier.pkl'
-with open(clf_path, 'rb') as f:
-    model.clf = pickle.load(f)
-    
-vec_path = 'src/lib/models/TFIDFVectorizer.pkl'
-with open(vec_path, 'rb') as f:
-    model.vectorizer = pickle.load(f)
-
-
+def isBase64(sb):
+    try:
+        if isinstance(sb, str):
+        # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
 
 
 class StrokeApiView(views.APIView):
@@ -41,30 +56,59 @@ class StrokeApiView(views.APIView):
 
 class PredictSentimentApiView(views.APIView):
     print("iiiiiiiiiiiiiiiiiiiiiiiii")
+    
+    global graph
+    graph = tf.get_default_graph()
+    
+    
+    
+    
     def get(self, request):
         
         print("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
         req = self.request
-        user_query = req.query_params.get('query')
-        print(req.query_params.get('query'))
-        print(request)
         
-        # vectorize the user's query and make a prediction
-        uq_vectorized = model.vectorizer_transform(np.array([user_query]))
-        prediction = model.predict(uq_vectorized)
-        pred_proba = model.predict_proba(uq_vectorized)
-        
-        # Output either 'Negative' or 'Positive' along with the score
-        if prediction == 0:
-            pred_text = 'Negative'
+        if request.META.get('CONTENT_TYPE', '').lower() == 'application/json' and len(request.body) > 0:
+            try:
+                body_data = json.loads(request.body)
+            except Exception as e:
+                return HttpResponseBadRequest(json.dumps({'error': 'Invalid request: {0}'.format(str(e))}), content_type="application/json")
         else:
-            pred_text = 'Positive'
-
-        # round the predict proba value and set to new variable
-        confidence = round(pred_proba[0], 3)
+            return HttpResponseBadRequest(json.dumps({'error': 'Invalid content type: shoud be application/json or body data is empty'}), content_type="application/json")
         
-        strokeApiResponseData = [{"likes": pred_text, "comments": confidence}]
-        results = {'prediction': pred_text, 'confidence': confidence}
+        
+        img_base64 = body_data["data"]
+        
+        if not isBase64(img_base64):
+            return HttpResponseBadRequest(json.dumps({'error': 'Image should be base64 encoded'}), content_type="application/json")
+        
+        
+        
+#         img_path = 'src/lib/data/images - 2019-08-23T131005.363.jpg'
+        
+#         with open('src/lib/data/images - 2019-08-23T131005.363.jpg','rb') as img_file:
+#             my_string = base64.b64encode(img_file.read())
+#             utf8_string = my_string.decode('utf-8')
+            
+#             print(utf8_string)
+        
+#         with open('src/lib/data/outimage.jpg', 'wb') as fh:
+#             fh.write(base64.b64decode(utf8_string))
+        
+        buf = io.BytesIO(base64.b64decode(img_base64))
+        img = Image.open(buf)
+        
+        img_cvt = cv2.cvtColor(array(img), cv2.COLOR_BGR2GRAY)
+        img_cvt = cv2.resize(img_cvt,(50,50))
+        img_cvt = img_cvt.reshape((1, 50 * 50))
+        img_cvt = img_cvt.astype('float32') / 255
+               
+        with graph.as_default():
+            preds = new_model.predict(img_cvt)
+        
+#         print(*preds, sep = ", ") 
+        
+        results = {'prediction': preds}
         return Response(results)
     
 
