4647f16824c766f0d1efccfbeebe4c9db93791b6
Dat Lam <lqdat2n@gmail.com>
2019-11-24 12:43:02 -0800

    Cleanup irrelevant code

--- src/strokerecognition/settings.py
+++ src/strokerecognition/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # new
-    'snippets.apps.SnippetsConfig', # new
     'strokeapi.apps.StrokeapiConfig', # new
     'corsheaders',
 ]
--- src/strokerecognition/urls.py
+++ src/strokerecognition/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')), # new
-    path('', include('snippets.urls')), # new
     path('stroke/', include('strokeapi.urls')), # new
 ]
